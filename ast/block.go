2cb7584f4aaacad7ac3b83beeda64a2c7ff362ff
Ali Nouri <alli.nouri.83@gmail.com>
2025-11-04 11:20:34 +0330

    parsing if expressions

---
+++ ast/block.go
@@ -0,0 +1,24 @@
+package ast
+
+import (
+	"bytes"
+
+	"monkey/token"
+)
+
+type BlockStatement struct {
+	Token      token.Token // the { token
+	Statements []Statement
+}
+
+func (bs *BlockStatement) statementNode()      {}
+func (bs *BlockStatement) TokenLiterl() string { return bs.Token.Literal }
+func (bs *BlockStatement) String() string {
+	var out bytes.Buffer
+
+	for _, s := range bs.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
