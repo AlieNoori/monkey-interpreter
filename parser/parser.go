d37fa7a3c81744bff764275e91925257eb049ade
Ali Nouri <alli.nouri.83@gmail.com>
2025-11-04 12:39:37 +0330

    parsing function literals

--- parser/parser.go
+++ parser/parser.go
@@ -66,6 +66,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.FALSE, p.parseBoolean)
 	p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
 	p.registerPrefix(token.IF, p.parseIfExpression)
+	p.registerPrefix(token.FUNCTION, p.parseFunctionLiteral)
 
 	// registering Prefix functions
 	p.registerInfix(token.PLUS, p.parseInfixExpression)
@@ -318,6 +319,74 @@ func (p *Parser) parseBlockStatement() *ast.BlockStatement {
 	}
 
 	return block
+}
+
+func (p *Parser) parseFunctionLiteral() ast.Expression {
+	lit := &ast.FunctionLiteral{Token: p.curToken}
+
+	if !p.expectPeek(token.LPAREN) {
+		return nil
+	}
+
+	lit.Parameters = p.parseFunctionParameters()
+
+	if !p.expectPeek(token.LBRACE) {
+		return nil
+	}
+
+	lit.Body = p.parseBlockStatement()
+
+	return lit
+}
+
+// func (p *Parser) parseFunctionParameters() []*ast.Identifier {
+// 	identifiers := []*ast.Identifier{}
+//
+// 	if p.peekTokenIs(token.RPAREN) {
+// 		p.nextToken()
+// 		return identifiers
+// 	}
+//
+// 	p.nextToken()
+//
+// 	ident := &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+// 	identifiers = append(identifiers, ident)
+//
+// 	for p.peekTokenIs(token.COMMA) {
+// 		p.nextToken()
+// 		p.nextToken()
+// 		ident := &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+// 		identifiers = append(identifiers, ident)
+// 	}
+//
+// 	if !p.expectPeek(token.RPAREN) {
+// 		return nil
+// 	}
+// 	return identifiers
+// }
+
+func (p *Parser) parseFunctionParameters() []*ast.Identifier {
+	params := []*ast.Identifier{}
+
+	if p.peekTokenIs(token.RPAREN) {
+		p.nextToken()
+		return params
+	}
+
+	for p.peekTokenIs(token.IDENT) {
+		p.nextToken()
+		param := p.parseIdentifier().(*ast.Identifier)
+		if params != nil {
+			params = append(params, param)
+		}
+		p.nextToken()
+	}
+
+	if !p.curTokenIs(token.RPAREN) {
+		return nil
+	}
+
+	return params
 }
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
